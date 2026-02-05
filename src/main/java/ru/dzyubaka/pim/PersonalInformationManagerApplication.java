e74f9d6b441cae0ae897bcaedfec695ec0006de7
Konstantin Dzyuba <dzyubaka3@gmail.com>
2026-02-06 01:23:37 +0300

    Initial commit

---
+++ src/main/java/ru/dzyubaka/pim/PersonalInformationManagerApplication.java
@@ -0,0 +1,13 @@
+package ru.dzyubaka.pim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalInformationManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalInformationManagerApplication.class, args);
+	}
+
+}

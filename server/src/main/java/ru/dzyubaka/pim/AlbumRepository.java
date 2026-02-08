cc4d1a153ead143df88867a5796f76b0bf706cd4
Konstantin Dzyuba <dzyubaka3@gmail.com>
2026-02-09 01:12:07 +0300

    Move to module

--- server/src/main/java/ru/dzyubaka/pim/AlbumRepository.java
+++ server/src/main/java/ru/dzyubaka/pim/AlbumRepository.java
@@ -1,6 +1,7 @@
 package ru.dzyubaka.pim;
 
 import org.springframework.data.repository.CrudRepository;
+import ru.dzyubaka.pim.Album;
 
 public interface AlbumRepository extends CrudRepository<Album, String> {
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,3 @@
 rootProject.name = "personal-information-manager"
+
+include("server")

c0edbb5d88978dc82f93bea80fa5627bc84cdf06
Konstantin Dzyuba <dzyubaka3@gmail.com>
2026-02-09 01:12:07 +0300

    Move to module

--- server/src/main/java/ru/dzyubaka/pim/AlbumController.java
+++ server/src/main/java/ru/dzyubaka/pim/AlbumController.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.dzyubaka.pim.Album;
+import ru.dzyubaka.pim.AlbumRepository;
 
 @RestController
 @RequestMapping("/albums")

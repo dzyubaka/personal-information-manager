5efaa1483966d8b45eadc80a2d6e2675b4b8527b
Konstantin Dzyuba <dzyubaka3@gmail.com>
2026-02-10 01:59:13 +0300

    Refactor album package structure

--- server/src/main/java/ru/dzyubaka/pim/controller/AlbumController.java
+++ server/src/main/java/ru/dzyubaka/pim/controller/AlbumController.java
@@ -1,11 +1,11 @@
-package ru.dzyubaka.pim;
+package ru.dzyubaka.pim.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.dzyubaka.pim.Album;
-import ru.dzyubaka.pim.AlbumRepository;
+import ru.dzyubaka.pim.model.Album;
+import ru.dzyubaka.pim.repository.AlbumRepository;
 
 @RestController
 @RequestMapping("/albums")
--- server/src/main/java/ru/dzyubaka/pim/repository/AlbumRepository.java
+++ server/src/main/java/ru/dzyubaka/pim/repository/AlbumRepository.java
@@ -1,7 +1,7 @@
-package ru.dzyubaka.pim;
+package ru.dzyubaka.pim.repository;
 
 import org.springframework.data.repository.CrudRepository;
-import ru.dzyubaka.pim.Album;
+import ru.dzyubaka.pim.model.Album;
 
 public interface AlbumRepository extends CrudRepository<Album, String> {
 }

a8d49bf02aa7e92aa092471a8aea18ea8081692e
Konstantin Dzyuba <dzyubaka3@gmail.com>
2026-02-11 01:56:07 +0300

    Remove credential store

--- client/src/main/java/ru/dzyubaka/pim/PersonalInformationManagerApplication.java
+++ client/src/main/java/ru/dzyubaka/pim/PersonalInformationManagerApplication.java
@@ -20,13 +20,10 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 import java.util.Base64;
 import java.util.List;
-import java.util.Properties;
 
 public class PersonalInformationManagerApplication extends Application {
 
@@ -37,28 +34,6 @@ public class PersonalInformationManagerApplication extends Application {
     @Override
     @SneakyThrows
     public void start(Stage primaryStage) {
-        Properties properties = load();
-        if (properties == null) {
-            primaryStage.setScene(createAuthorizationScene(primaryStage));
-        } else {
-            String username = (String) properties.get("username");
-            String password = (String) properties.get("password");
-            HttpResponse<String> response = authorize(username, password);
-            if (response.statusCode() == 401) {
-                primaryStage.setScene(createAuthorizationScene(primaryStage));
-                showUnauthorizedError();
-                Files.delete(Path.of(".properties"));
-            } else {
-                store(username, password);
-                List<Album> albums = new JsonMapper().readValue(response.body(), new TypeReference<>() {});
-                primaryStage.setScene(createAlbumsScene(albums));
-            }
-        }
-        primaryStage.setTitle("Personal Information Manager");
-        primaryStage.show();
-    }
-
-    private Scene createAuthorizationScene(Stage stage) {
         TextField usernameField = new TextField();
         usernameField.setPromptText("Username");
         TextField passwordField = new TextField();
@@ -66,19 +41,18 @@ public class PersonalInformationManagerApplication extends Application {
         Button authorizeButton = new Button("Authorize");
         authorizeButton.setMaxWidth(Double.MAX_VALUE);
         authorizeButton.setOnAction(event -> {
-            String username = usernameField.getText();
-            String password = passwordField.getText();
-            HttpResponse<String> response = authorize(username, password);
+            HttpResponse<String> response = authorize(usernameField.getText(), passwordField.getText());
             if (response.statusCode() == 401) {
                 showUnauthorizedError();
             } else {
-                store(username, password);
                 List<Album> albums = new JsonMapper().readValue(response.body(), new TypeReference<>() {});
-                stage.setScene(createAlbumsScene(albums));
-                stage.centerOnScreen();
+                primaryStage.setScene(createAlbumsScene(albums));
+                primaryStage.centerOnScreen();
             }
         });
-        return new Scene(new VBox(usernameField, passwordField, authorizeButton));
+        primaryStage.setScene(new Scene(new VBox(usernameField, passwordField, authorizeButton)));
+        primaryStage.setTitle("Personal Information Manager");
+        primaryStage.show();
     }
 
     private Scene createAlbumsScene(List<Album> albums) {
@@ -101,24 +75,6 @@ public class PersonalInformationManagerApplication extends Application {
         Alert alert = new Alert(Alert.AlertType.ERROR);
         alert.setHeaderText("Unauthorized");
         alert.showAndWait();
-    }
-
-    @SneakyThrows
-    private static void store(String username, String password) {
-        Properties properties = new Properties();
-        properties.put("username", username);
-        properties.put("password", password);
-        properties.store(Files.newOutputStream(Path.of(".properties")), null);
-    }
-
-    @SneakyThrows
-    private static Properties load() {
-        Path path = Path.of(".properties");
-        if (Files.exists(path)) {
-            Properties properties = new Properties();
-            properties.load(Files.newInputStream(path));
-            return properties;
-        } else return null;
     }
 
     @SneakyThrows

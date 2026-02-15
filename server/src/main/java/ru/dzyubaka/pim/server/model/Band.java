32362ca2c1bd94da8e193bf079a650f6029e7070
Konstantin Dzyuba <dzyubaka3@gmail.com>
2026-02-15 23:00:56 +0300

    Add one-to-many relationship

--- server/src/main/java/ru/dzyubaka/pim/server/model/Band.java
+++ server/src/main/java/ru/dzyubaka/pim/server/model/Band.java
@@ -1,10 +1,10 @@
 package ru.dzyubaka.pim.server.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
 import lombok.Data;
+
+import java.util.List;
 
 @Data
 @Entity
@@ -15,4 +15,8 @@ public class Band {
     private Long id;
 
     private String name;
+
+    @OneToMany(mappedBy = "band")
+    @JsonManagedReference
+    private List<Album> albums;
 }

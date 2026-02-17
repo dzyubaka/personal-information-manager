7baca0907f98f207f1eb0688f91278898d9a6ee9
Konstantin Dzyuba <dzyubaka3@gmail.com>
2026-02-18 00:20:28 +0300

    Add JWT authentication

---
+++ server/src/main/java/ru/dzyubaka/pim/server/security/SecurityConfig.java
@@ -0,0 +1,28 @@
+package ru.dzyubaka.pim.server.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+        return http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth ->
+                        auth.requestMatchers("/auth").permitAll().anyRequest().authenticated())
+                .addFilterBefore(new AuthFilter(), UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new InMemoryUserDetailsManager(User.withUsername("username").password("{noop}password").build());
+    }
+}

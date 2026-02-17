f937109b34231882adf3e8cd17ce9e810bfb3898
Konstantin Dzyuba <dzyubaka3@gmail.com>
2026-02-18 01:45:48 +0300

    Refactor client app

--- client/src/main/java/ru/dzyubaka/pim/client/PersonalInformationManagerApplication.java
+++ client/src/main/java/ru/dzyubaka/pim/client/PersonalInformationManagerApplication.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.CheckBoxListCell;
@@ -15,7 +16,6 @@ import tools.jackson.core.type.TypeReference;
 import tools.jackson.databind.ObjectMapper;
 import tools.jackson.databind.json.JsonMapper;
 
-import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -24,6 +24,7 @@ import java.util.List;
 
 public class PersonalInformationManagerApplication extends Application {
     private static final HttpClient CLIENT = HttpClient.newHttpClient();
+    private static final ObjectMapper MAPPER = new JsonMapper();
 
     public static void main(String[] args) {
         launch(args);
@@ -31,55 +32,29 @@ public class PersonalInformationManagerApplication extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        TextField usernameField = new TextField("username");
-        usernameField.setPromptText("Username");
-        TextField passwordField = new TextField("password");
-        passwordField.setPromptText("Password");
+        showAuthScene(primaryStage);
+        primaryStage.setTitle("Personal Information Manager");
+        primaryStage.show();
+    }
+
+    private static void showAuthScene(Stage stage) {
+        TextField username = new TextField("username");
+        username.setPromptText("Username");
+        TextField password = new TextField("password");
+        password.setPromptText("Password");
         Button authorizeButton = new Button("Authorize");
         authorizeButton.setMaxWidth(Double.MAX_VALUE);
         authorizeButton.setOnAction(event -> {
-            try {
-                String token = auth(usernameField.getText(), passwordField.getText());
-                if (token == null) {
-                    Alert alert = new Alert(Alert.AlertType.ERROR);
-                    alert.setHeaderText("Unauthorized");
-                    alert.showAndWait();
-                } else {
-                    HttpRequest bandsRequest = HttpRequest.newBuilder(URI.create("http://localhost:8080/bands"))
-                            .header("Authorization", "Bearer " + token).build();
-                    HttpResponse<String> bandsResponse = CLIENT.send(bandsRequest, HttpResponse.BodyHandlers.ofString());
-                    ObjectMapper mapper = new JsonMapper();
-                    List<Band> bands = mapper.readValue(bandsResponse.body(), new TypeReference<>() {});
-                    ListView<Band> bandsListView = new ListView<>(FXCollections.observableList(bands));
-                    Scene scene = new Scene(bandsListView, 640, 400);
-                    bandsListView.getSelectionModel().selectedItemProperty().addListener((ObservableValue<? extends Band> observable, Band oldValue, Band newValue) -> {
-                        try {
-                            HttpRequest bandRequest = HttpRequest.newBuilder(URI.create("http://localhost:8080/bands/" + newValue.id()))
-                                    .header("Authorization", "Bearer " + token).build();
-                            HttpResponse<String> response = CLIENT.send(bandRequest, HttpResponse.BodyHandlers.ofString());
-                            Band band = mapper.readValue(response.body(), new TypeReference<>() {});
-                            ListView<Album> listView = new ListView<>(FXCollections.observableList(band.albums()));
-                            listView.setCellFactory(CheckBoxListCell.forListView(a -> new SimpleBooleanProperty(a.getListenedAt() != null)));
-                            BorderPane root = new BorderPane();
-                            Button button = new Button("Back");
-                            button.setOnAction(event1 -> scene.setRoot(bandsListView));
-                            root.setTop(new ToolBar(button));
-                            root.setCenter(listView);
-                            scene.setRoot(root);
-                        } catch (IOException | InterruptedException e) {
-                            throw new RuntimeException(e);
-                        }
-                    });
-                    primaryStage.setScene(scene);
-                    primaryStage.centerOnScreen();
-                }
-            } catch (IOException | InterruptedException e) {
-                    throw new RuntimeException(e);
+            String token = auth(username.getText(), password.getText());
+            if (token == null) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setHeaderText("Unauthorized");
+                alert.showAndWait();
+            } else {
+                showBandsScene(stage, token);
             }
         });
-        primaryStage.setScene(new Scene(new VBox(usernameField, passwordField, authorizeButton)));
-        primaryStage.setTitle("Personal Information Manager");
-        primaryStage.show();
+        stage.setScene(new Scene(new VBox(username, password, authorizeButton)));
     }
 
     @SneakyThrows
@@ -90,5 +65,36 @@ public class PersonalInformationManagerApplication extends Application {
                 .build();
         HttpResponse<String> response = CLIENT.send(request, HttpResponse.BodyHandlers.ofString());
         return response.statusCode() == 401 ? null : response.body();
+    }
+
+    @SneakyThrows
+    private static void showBandsScene(Stage stage, String token) {
+        HttpRequest bandsRequest = HttpRequest.newBuilder(URI.create("http://localhost:8080/bands"))
+                .header("Authorization", "Bearer " + token).build();
+        HttpResponse<String> bandsResponse = CLIENT.send(bandsRequest, HttpResponse.BodyHandlers.ofString());
+        List<Band> bands = MAPPER.readValue(bandsResponse.body(), new TypeReference<>() {
+        });
+        ListView<Band> bandsListView = new ListView<>(FXCollections.observableList(bands));
+        Scene scene = new Scene(bandsListView, 640, 400);
+        bandsListView.getSelectionModel().selectedItemProperty().addListener((ObservableValue<? extends Band> observable, Band oldValue, Band newValue) ->
+                showBandView(scene, newValue.id(), token, bandsListView));
+        stage.setScene(scene);
+        stage.centerOnScreen();
+    }
+
+    @SneakyThrows
+    private static void showBandView(Scene scene, long bandId, String token, Parent backView) {
+        HttpRequest bandRequest = HttpRequest.newBuilder(URI.create("http://localhost:8080/bands/" + bandId))
+                .header("Authorization", "Bearer " + token).build();
+        HttpResponse<String> response = CLIENT.send(bandRequest, HttpResponse.BodyHandlers.ofString());
+        Band band = MAPPER.readValue(response.body(), new TypeReference<>() {});
+        ListView<Album> listView = new ListView<>(FXCollections.observableList(band.albums()));
+        listView.setCellFactory(CheckBoxListCell.forListView(a -> new SimpleBooleanProperty(a.getListenedAt() != null)));
+        BorderPane root = new BorderPane();
+        Button button = new Button("Back");
+        button.setOnAction(event -> scene.setRoot(backView));
+        root.setTop(new ToolBar(button));
+        root.setCenter(listView);
+        scene.setRoot(root);
     }
 }

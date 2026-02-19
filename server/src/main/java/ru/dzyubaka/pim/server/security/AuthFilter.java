4360dae2cce1c009e9b87718a1801c9498dbb8fd
Konstantin Dzyuba <dzyubaka3@gmail.com>
2026-02-20 02:31:25 +0300

    Sign JWT with HMAC-SHA

--- server/src/main/java/ru/dzyubaka/pim/server/security/AuthFilter.java
+++ server/src/main/java/ru/dzyubaka/pim/server/security/AuthFilter.java
@@ -1,10 +1,13 @@
 package ru.dzyubaka.pim.server.security;
 
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -17,11 +20,15 @@ import java.util.Collections;
 
 @Component
 public class AuthFilter extends OncePerRequestFilter {
+
+    @Value("${jwt.secret}")
+    private String jwtSecretKey;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String header = request.getHeader(HttpHeaders.AUTHORIZATION);
         if (header != null) {
-            String principal = Jwts.parser().unsecured().build().parseUnsecuredClaims(header.substring(7)).getPayload().getSubject();
+            String principal = Jwts.parser().verifyWith(Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecretKey))).build().parseSignedClaims(header.substring(7)).getPayload().getSubject();
             UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(principal, null, Collections.emptyList());
             authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
             SecurityContextHolder.getContext().setAuthentication(authentication);
